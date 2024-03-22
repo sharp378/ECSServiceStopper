dcddebdd1dcb9a780659348483a358e8350302ec
sharp378 <107257160+sharp378@users.noreply.github.com>
2024-03-22 08:30:28 -0400

    refactor again to use annotations (#3)
    * refactor again to use annotations
    
    * remove unused package
    
    * remove api def

--- src/ECSServiceStopper/Functions.cs
+++ src/ECSServiceStopper/Functions.cs
@@ -1,22 +1,27 @@
+using Amazon.Lambda.Core;
+using Amazon.Lambda.Annotations;
+using Amazon.Lambda.Annotations.APIGateway;
 using Amazon.ECS;
 using Amazon.ECS.Model;
-using Amazon.Lambda.Core;
-using Microsoft.Extensions.DependencyInjection;
+using Threading=System.Threading.Tasks;
 
-// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
 namespace ECSServiceStopper;
 
-public class Function(IAmazonECS client)
+/// <summary>
+/// Holds function handlers for Lambda invocations 
+/// </summary>
+public class Functions(IAmazonECS client)
 {
     /// <summary>
     /// Stops an ECS service by setting its desired count to zero.
     /// </summary>
     /// <param name="input">(UNUSED) The event for the Lambda function handler to process.</param>
     /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
-    /// <returns></returns> 
-    public async System.Threading.Tasks.Task FunctionHandler(string input, ILambdaContext context)
+    /// <returns></returns>
+    [LambdaFunction]
+    public async Threading.Task StopECSServiceAsync(string input, ILambdaContext context)
     {
         var cluster = Environment.GetEnvironmentVariable("ECS_CLUSTER")
             ?? throw new NullReferenceException("ECS_CLUSTER environment variable is required");
@@ -32,13 +37,5 @@ public class Function(IAmazonECS client)
 
         var token = new CancellationToken();
         await client.UpdateServiceAsync(request, token);
-    }
-}
-
-public class Startup
-{
-    public void ConfigureServices(IServiceCollection services)
-    {
-        services.AddAWSService<IAmazonECS>();
     }
 }
